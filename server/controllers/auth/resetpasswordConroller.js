37d5939492fa710968cb52af0ad5b0fb8215daae
hardynavadiya <hardinavdiya@gmail.com>
2022-01-12 11:58:33 +0530

    reset password with email

--- server/controllers/auth/resetpasswordConroller.js
+++ server/controllers/auth/resetpasswordConroller.js
@@ -3,7 +3,7 @@ import CustomErrorHandler from '../../services/CustomErrorHandler';
 import {User} from "../../models"
 import Joi from "joi"
 import bcrypt from "bcryptjs";
-
+import sendEmail from '../../sendEmail/sendmail';
 
 const resetpasswordController = {
 
@@ -40,15 +40,16 @@ const resetpasswordController = {
 				.pattern(new RegExp('^[a-zA-Z0-9]{3,20}$'))
 				.required(),
 			repeat_password: Joi.ref('password'),
-		});
-
+		});	
 		const { error } = PasswordSchema.validate(req.body);
-
 		if (error) {
 			return next(error);
 			// throw error;
 		}
+
+		// password save in database
 		const {password} = req.body;
+		const message = "your password reset sucessfully";
 		try{
 			const hashedPassword = await bcrypt.hash(password, 10);
 			console.log(hashedPassword);
@@ -56,6 +57,11 @@ const resetpasswordController = {
 			userPass.resetPasswordToken = undefined;
 			userPass.resetPasswordExpire = undefined;
 			await userPass.save()
+			await sendEmail({
+				email: userPass.email,
+				subject: `Reset Passsowrd is change`,
+				message,
+			});
 			return res.json({
 				statusCode:200,
 				message:"Your Password Change Sucessfully",
